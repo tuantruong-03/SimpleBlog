b71b5697432b069540679d09219fb6b0874aa7e3
tay <phuonggteyy@gmail.com>
2024-06-27 10:51:13 +0700

    #1-Tay: Add Login and Register into frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Login from './component/security/Login';
+import Register from './component/security/Register';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +11,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <Login /> */}
+    <Register/>
   </React.StrictMode>
 );
 
