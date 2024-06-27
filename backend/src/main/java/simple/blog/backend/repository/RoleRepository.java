cdd938ebec2213ede289f8c21d07bb867fe46b74
tay <phuonggteyy@gmail.com>
2024-06-27 11:17:52 +0700

    uri: mongodb+srv://tayphuong1109:PhuongTay110903%40@javaphuongtay.qkqk0o3.mongodb.net/?ssl=true&retryWrites=true&w=majority&appName=JavaPhuongTay

--- backend/src/main/java/simple/blog/backend/repository/RoleRepository.java
+++ backend/src/main/java/simple/blog/backend/repository/RoleRepository.java
@@ -1,9 +1,11 @@
 package simple.blog.backend.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import simple.blog.backend.model.Role;
 
+@Repository
 public interface RoleRepository extends MongoRepository<Role, Integer> {
     public Role findByAuthority(String authority);
     
--- backend/src/main/java/simple/blog/backend/repository/UserRepository.java
+++ backend/src/main/java/simple/blog/backend/repository/UserRepository.java
@@ -3,9 +3,11 @@ package simple.blog.backend.repository;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import simple.blog.backend.model.User;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, Integer> {
     public User findByUsername(String username);
     public User findByUserId(Integer userId);
