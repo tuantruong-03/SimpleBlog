ab3eaa307247bcac40af6f80810ff31eb1f4d546
tay <phuonggteyy@gmail.com>
2024-06-28 16:10:47 +0700

    #3-Tay: Create EmailVerificationToken document

---
+++ backend/src/main/java/simple/blog/backend/model/EmailVerificationToken.java
@@ -0,0 +1,21 @@
+package simple.blog.backend.model;
+
+import java.util.Date;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@Document("email_verification_tokens")
+public class EmailVerificationToken extends AbstractEntity {
+
+	 private String userEmail;
+	 private String token;
+	 private Date expiryDate;
+
+}
